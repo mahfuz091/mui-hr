9ab6aa69350f543d98d3dbac8c55e82ee9507def
mahfuz091 <engmahfuz14@gmail.com>
2024-04-08 19:49:26 +0600

    contact section added

--- src/app/components/Personal/ContactCard.jsx
+++ src/app/components/Personal/ContactCard.jsx
@@ -1,10 +1,68 @@
 "use client";
+import { HrContext } from "@/context/HrProvider";
 import { Box, Button, Card, TextField, Typography, Grid } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
 import { MdEdit } from "react-icons/md";
+import axiosInstance from "@/lib/axios-instance";
 
 const ContactCard = () => {
+  const { control, setControl } = useContext(HrContext);
   const [isEditing, setEditing] = useState(false);
+  const [contact, setContact] = useState(null);
+  console.log(contact);
+  const getContact = async () => {
+    const token = localStorage.getItem("accessToken");
+    try {
+      const response = await axiosInstance.get("/api/profile/contacts", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = response.data.data.contacts;
+      setContact(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getContact();
+  }, [control]);
+
+  const handleContact = async (e) => {
+    e.preventDefault();
+
+    const mobile_number = e.target.mobile_number.value;
+    const work_phone_number = e.target.work_phone_number.value;
+    const skype = e.target.skype.value;
+    const discord = e.target.discord.value;
+
+    const contact = {
+      mobile_number,
+      work_phone_number,
+      skype,
+      discord,
+    };
+    const token = localStorage.getItem("accessToken");
+    try {
+      const response = await axiosInstance.post(
+        "/api/profile/contacts",
+        contact,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = response.data.data;
+
+      setControl(!control);
+      setEditing(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Card
       sx={{
@@ -34,33 +92,41 @@ const ContactCard = () => {
       </Box>
       {isEditing ? (
         <Box sx={{ padding: "20px" }}>
-          <form action=''>
+          <form action='' onSubmit={handleContact}>
             <TextField
               fullWidth
               margin='normal'
-              placeholder='Mobile No'
+              placeholder={contact?.mobile_number || "Mobile No"}
               label='Mobile No'
+              defaultValue={contact?.mobile_number}
+              name='mobile_number'
             ></TextField>
             <TextField
               fullWidth
               margin='normal'
-              placeholder='Work Phone No'
+              placeholder={contact?.work_phone_number || "Work Phone No"}
               label='Work Phone No'
+              defaultValue={contact?.work_phone_number}
+              name='work_phone_number'
             ></TextField>
             <TextField
               fullWidth
               margin='normal'
-              placeholder='Skype Username'
+              placeholder={contact?.skype || "Skype Username"}
               label='Skype Username'
+              defaultValue={contact?.skype}
+              name='skype'
             ></TextField>
             <TextField
               fullWidth
               margin='normal'
-              placeholder='Discord Username'
+              placeholder={contact?.discord || "Discord Username"}
               label='Discord Username'
+              defaultValue={contact?.discord}
+              name='discord'
             ></TextField>
             <Box sx={{ textAlign: "right", marginTop: "10px" }}>
-              <Button variant='contained' onClick={() => setEditing(false)}>
+              <Button variant='contained' type='submit'>
                 Save
               </Button>
             </Box>
@@ -78,10 +144,12 @@ const ContactCard = () => {
           </Grid>
           <Grid item xs={8}>
             <Box sx={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-              <Typography variant='body2'>Mobile No</Typography>
-              <Typography variant='body2'>Work Phone No</Typography>
-              <Typography variant='body2'>Skype Username</Typography>
-              <Typography variant='body2'>Discord Username</Typography>
+              <Typography variant='body2'>{contact?.mobile_number}</Typography>
+              <Typography variant='body2'>
+                {contact?.work_phone_number}
+              </Typography>
+              <Typography variant='body2'>{contact?.skype}</Typography>
+              <Typography variant='body2'>{contact?.discord}</Typography>
             </Box>
           </Grid>
         </Grid>
